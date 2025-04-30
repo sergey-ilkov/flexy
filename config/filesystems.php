81853a282b7aebba46926d1a0589e93e7f40bb21
Sergey <ilkov1103@gmail.com>
2025-04-30 12:24:29 +0300

    test

--- config/filesystems.php
+++ config/filesystems.php
@@ -73,7 +73,7 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
 
-        base_path('flexy') => base_path('public'),
+        base_path('public_html') => base_path('public'),
     ],
 
 ];
